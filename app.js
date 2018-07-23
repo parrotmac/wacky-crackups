2688ea21ee43cf82b6804839de8c2c8d443ee14f
Isaac Parker <parrotmac@gmail.com>
2018-07-22 23:16:01 -0600

    Adding express-handlebars to support easy partials

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+const expressHandlebarsConfig = {
+  layoutsDir: 'views/layouts/',
+    defaultLayout: 'layout.hbs',
+    extname: '.hbs',
+    partialsDir: "views/partials/"
+};
+app.engine('hbs', exphbs(expressHandlebarsConfig));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
